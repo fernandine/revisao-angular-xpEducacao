97164db5b120b231e921e73dee02c24e6c1cc7bf
Jean Fernandine <jfernandine@gmail.com>
2022-10-25 08:40:52 -0300

    aula 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { C2Component } from './c2/c2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    C2Component
   ],
   imports: [
     BrowserModule
